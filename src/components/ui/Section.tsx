632cf5396b65fb9489076436bb2412744c3bcf6d
tomtomdu73 <thomas.cosialls@gmail.com>
2024-05-22 12:49:20 -0500

    feat(section): add inView effect on sections

---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react'
+import { useInView } from 'framer-motion'
+
+interface SectionProps {
+  children: React.ReactNode
+  id: string
+  className?: string
+}
+
+const Section = ({ children, id, className }: SectionProps) => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
+  return (
+    <section
+      ref={ref}
+      id={id}
+      style={{
+        transform: isInView ? 'none' : 'translateY(-200px)',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+      }}
+      className={className}
+    >
+      {children}
+    </section>
+  )
+}
+
+export default Section

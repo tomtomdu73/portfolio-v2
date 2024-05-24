c33db7dc14e1a2ae1ac29d2c12bff4a4df07ae09
tomtomdu73 <thomas.cosialls@gmail.com>
2024-05-23 20:12:31 -0500

    feat: setup sanity

---
+++ src/sanity/schemaTypes/stack.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'stack',
+  title: 'Stack',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        },
+      ],
+    }),
+  ],
+})

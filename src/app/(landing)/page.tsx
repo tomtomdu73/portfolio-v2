5005450f472007c78f09d8eed76ddc3e268ad5df
tomtomdu73 <thomas.cosialls@gmail.com>
2024-07-04 18:20:35 -0500

    perf(backgroundimage): use blur preload

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -31,30 +31,7 @@ export default function Home() {
       <Clients />
       <Products />
       <Experiences />
-      {/* 
-      <div className="content__text">
-        <p className="right">
-          At <strong>Nova</strong>Motion&reg;, our team of creative artists and tech experts work
-          together to explore new possibilities. Each project shows our dedication to new ideas and
-          high quality, whether it's a commercial, film, or interactive display.
-        </p>
-        <p className="highlight">
-          We believe motion design can tell stories, evoke emotions, and inspire imaginations.
-        </p>
-        <p>
-          Our clients are at the core of everything we do at <strong>Nova</strong>Motion&reg;. We
-          pride ourselves on building strong, lasting relationships based on trust, communication,
-          and mutual respect. We listen carefully to your needs and goals, ensuring we fully
-          understand your vision.{' '}
-        </p>
-        <p>
-          By collaborating closely, we tailor our approach to meet your unique requirements,
-          delivering results that exceed expectations. Whether you are a small business or a large
-          corporation, we are committed to helping you succeed. With <strong>Nova</strong>
-          Motion&reg;, you are not just a client; you are a valued partner in creating exceptional
-          visual stories.
-        </p>
-      </div> */}
+
       <footer className="content__footer">
         <span>Made by Thomas Cosialls</span>
         <ExternalLink title="Animation inspired by Codrops" url="https://tympanus.net/codrops/" />
